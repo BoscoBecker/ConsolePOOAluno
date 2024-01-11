4c231b2c242b13d976dbcdff9accc5400fef8975
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-11 19:30:37 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace ConsolePOOAluno.Entidade
+  
+{
+  internal class Aluno
+  {
+    public string Nome;
+    public double Nota;
+    public bool Aprovado;
+
+    public double Resultado() {
+      this.Aprovado = (this.Nota >= 60.0);
+      if (!Aprovado)
+        this.Nota -= 60;
+      return this.Nota;
+    }
+
+    public override string ToString()
+    {
+      string resultado;
+      Resultado();
+      if (this.Aprovado)
+        resultado = "NOTA FINAL = " + this.Nota + " APROVADO "+ this.Nota + " PONTOS";
+      else
+        resultado = "NOTA FINAL = " + this.Nota + " REPROVADO" + " FALTARAM" + this.Nota +" PONTOS";
+
+      return resultado;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsolePOOAluno.Entidade;
+using System.Globalization;
+
+Aluno aluno = new Aluno();
+Console.Write("Informe o nome do aluno: ");
+aluno.Nome = Console.ReadLine();
+
+Console.Write("Informe a primeira nota: ");
+aluno.Nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Informe a Segunda nota: ");
+aluno.Nota += double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Informe a terceira nota: ");
+aluno.Nota += double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine(aluno);
+
